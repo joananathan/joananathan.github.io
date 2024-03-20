eae4bcf5f6b05ab780afc3bcaf6786afc32f23c7
Jonathan Zhu <jonathan.zhu5@education.nsw.gov.au>
2024-03-20 23:01:52 +1100

    progress

--- agenda/script.js
+++ agenda/script.js
@@ -1,9 +1,21 @@
-const renderCategrory =() => {
-    const categroryList = document.querySelector('.categrory-list');
-    categroryList.innerHTML = '';
-    for (let i = 0; i < categrory.length; i++) {
-        const categroryItem = document.createElement('li');
-        categroryItem.innerHTML = categrory[i];
-        categroryList.appendChild(categroryItem);
+const categoriesContainer = document.querySelector('[category-list]');
+
+let categories = ['name', 'todo']
+
+function render() {
+    clearElement(categoriesContainer);
+    categories.forEach(category => {
+        const categoryElement = document.createElement('li')
+        categoryElement.classList.add('category')
+        categoryElement.textContent = category
+        categoriesContainer.appendChild(categoryElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
     }
 }
+
+render()
