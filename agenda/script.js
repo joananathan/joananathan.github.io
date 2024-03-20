074361be46d5d672c2c94a06c3c42b3fb41b7f74
joananathan <158416433+joananathan@users.noreply.github.com>
2024-03-21 10:44:26 +1100

    start javascript

--- agenda/script.js
+++ agenda/script.js
@@ -1,13 +1,41 @@
 const categoriesContainer = document.querySelector('[category-list]');
+const newCategoryForm = document.querySelector('[data-new-category-form]');
+const newCategoryInput = document.querySelector('[data-new-category-input]');
 
-let categories = ['name', 'todo']
+const LOCAL_STORAGE_CATEGORY_KEY = 'task.categories'
+
+let categories = JSON.parse(localStorage.getItem(LOCAL_STORAGE_CATEGORY_KEY)) || []
+
+newCategoryForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const categoryName = newCategoryInput.value
+    if (categoryName == null || categoryName === '') return
+    const category = createCategory(categoryName)
+    newCategoryInput.value = null
+    categories.push(category)
+    saveAndRender()
+})
+
+function createCategory(name) {
+    return {id: Date.now().toString(), name: name, tasks: []}
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_CATEGORY_KEY, JSON.stringify(categories))
+}
 
 function render() {
     clearElement(categoriesContainer);
     categories.forEach(category => {
         const categoryElement = document.createElement('li')
+        categoryElement.dataset.categoryId = category.id
         categoryElement.classList.add('category')
-        categoryElement.textContent = category
+        categoryElement.textContent = category.name
         categoriesContainer.appendChild(categoryElement)
     })
 }
